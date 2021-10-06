3bf7b101d1ca95c1a2f2b28ba1bf39cadd380ca5
Sophie Traen <sophie_traen@yahoo.com>
2021-10-06 09:31:54 +0200

    added custom styling

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import "!style-loader!css-loader!./styles.css";
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
@@ -11,4 +12,4 @@ export const parameters = {
     },
   },
   docs: { inlineStories: true },
-}
+};
